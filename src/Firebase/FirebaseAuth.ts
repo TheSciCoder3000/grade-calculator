ad68b76cba1ed34a477209445e33d2babbcbbbb0
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-05-31 11:13:47 +0800

    file moved to upper directory

---
+++ src/Firebase/FirebaseAuth.ts
@@ -0,0 +1,78 @@
+import { FirebaseApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, User, signOut, signInWithEmailAndPassword } from 'firebase/auth'
+import React from 'react'
+
+
+// ================================== Type Definitions ==================================
+/**
+ * initializes the firebase authentication and return an object containing all the auth-related functions such as:
+ * - signing up
+ * - logging in
+ * - signing out
+ * - status listener
+ * @param app an instance of the FirebaseApp
+ * @param createUserDb 
+ * @returns an object containing all the Authetication-related functions
+ */
+export function InitializeAuthentication(app: FirebaseApp, createUserDb: (userUid: string) => Promise<void>) {
+    const auth = getAuth(app)
+
+    /**
+     * creates an event listener that runs a dispatch function when changes occur
+     * @param setUserStatus a dispatch function from a react state 
+     * @returns an UnSubscribe function
+     */
+    const AuthStatusListener = (setUserStatus: React.Dispatch<React.SetStateAction<User | null>>) => auth.onAuthStateChanged(user => setUserStatus(user))
+
+    /**
+     * used to create a user upon filling up the sign up form
+     * @param email 
+     * @param password 
+     * @returns the user's credentials when successful or an error message when failure
+     */
+    const AuthSignUp = async (email: string, password: string) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log(userCredential)
+                createUserDb(userCredential.user.uid)
+                return userCredential.user
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(`${errorCode}: ${errorMessage}`)
+                return null
+            });
+    }
+
+    /**
+     * used to sign out the current user
+     * @returns a promise of the sign out procedure
+     */
+    const AuthSignOut = async () => {
+        console.log('signing out')
+        return signOut(auth)
+    }
+
+    /**
+     * Logs in the user using ther email and password
+     * @param email 
+     * @param password 
+     * @returns the user's credentials when successful or an error response when failure
+     */
+    const AuthLogIn = async (email: string, password: string) => {
+        return signInWithEmailAndPassword(auth, email, password)
+            .then((userCredentials) => {
+                console.log('User has successfuly logged in')
+                return userCredentials.user
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(`${errorCode}: ${errorMessage}`)
+                return null
+            })
+    }
+
+    return { AuthSignUp, AuthLogIn, AuthSignOut, AuthStatusListener }
+}
---
+++ src/Firebase/index.tsx
@@ -0,0 +1,219 @@
+import { createContext, useContext, useEffect, useState, useCallback } from "react";
+
+import { initializeApp } from "firebase/app";
+import { User } from 'firebase/auth'
+// import { getAnalytics } from "firebase/analytics";
+
+import { InitializeAuthentication } from "./FirebaseAuth";
+import { initializeFirestore, IUserDoc } from "./FirebaseDb";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDR7cy9pg18k1hEhbKkFgG9wNM5T6CcFvY",
+  authDomain: "grade-calculator-3000.firebaseapp.com",
+  projectId: "grade-calculator-3000",
+  storageBucket: "grade-calculator-3000.appspot.com",
+  messagingSenderId: "565775310347",
+  appId: "1:565775310347:web:7d2abca2387076bf45b10e",
+  measurementId: "G-8DV6VP5YXG"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const FirebaseDb = initializeFirestore(app)
+const FirebaseAuth = InitializeAuthentication(app, FirebaseDb.createUserDb)
+
+
+// ===================================== Firebase Context Values =====================================
+/**
+ * creates a dictionary of the user's authentication with auth-related functions
+ * Functions include:
+ * - signing up
+ * - signing out
+ * - loging in
+ * - listening to authentication changes
+ */
+const useAuthContext = () => {
+  // auth module states
+  const [AuthStatus, setAuthStatus] = useState<User | null>(null)
+
+  // Add auth status listener after rendering component
+  useEffect(() => FirebaseAuth.AuthStatusListener(setAuthStatus), [])
+
+
+  return {
+    AuthStatus,
+    AuthFunctions: {
+      AuthSignUp: FirebaseAuth.AuthSignUp,
+      AuthSignOut: FirebaseAuth.AuthSignOut,
+      AuthLogIn: FirebaseAuth.AuthLogIn,
+      AuthListener: FirebaseAuth.AuthStatusListener
+    }
+  }
+}
+
+/**
+ * creates a dictionary of the data from the database and database-related functions such as:
+ * - Adding documents
+ * - removing documents
+ * - updating documents
+ * - listening to changes
+ */
+const useFirestoreContext = () => {
+  // Firestore module state
+  const [FirestoreDb, setFirestoreDb] = useState<IUserDoc | undefined>({} as IUserDoc)
+  console.log('testing usefirestore', FirestoreDb)
+  
+
+  const updateFirestoreDb = useCallback((userUid: string) => {
+    return FirebaseDb.onDbChanges(userUid, setFirestoreDb)
+  }, [])
+
+  return [
+    {
+      Firestore: FirestoreDb,
+      FirestoreFunctions: {
+        AddDocs: null,
+        RemoveDocs: null,
+        UpdateDocs: null,
+        onDocsChange: null
+      }
+    },
+    updateFirestoreDb
+  ] 
+  
+}
+
+// ===================================== Firebase Context Creation =====================================
+type FireContextReturnType = ReturnType<typeof useFirestoreContext>
+type FireContextItemArrayType = FireContextReturnType[1]
+type FireFunc = Extract<FireContextItemArrayType, Function>
+type FireObj = Exclude<FireContextItemArrayType, Function>
+type FirestoreContextType = [FireObj, FireFunc]
+
+export interface IFirebaseContext {
+  Auth: ReturnType<typeof useAuthContext>
+  Firestore: FirestoreContextType[0]
+}
+
+/**
+ * firebase context for providing states across nested components
+ */
+const FirebaseContext = createContext<IFirebaseContext>({
+  Auth: {} as ReturnType<typeof useAuthContext>,
+  Firestore: {} as FirestoreContextType[0]
+})
+
+
+interface IFirebaseContextProvider {
+  children: JSX.Element
+}
+
+/**
+ * Firebase Context provideer component
+ */
+export const FirebaseConetxtProvider: React.FC<IFirebaseContextProvider> = ({ children }) => {
+  const FirebaseAuth = useAuthContext()
+  const [Firestore, updateFirestore] = useFirestoreContext() as FirestoreContextType
+
+  useEffect(() => {
+    if (FirebaseAuth.AuthStatus) return updateFirestore(FirebaseAuth.AuthStatus.uid)
+  }, [FirebaseAuth.AuthStatus, updateFirestore])
+  return (
+    <FirebaseContext.Provider value={{
+      Auth: FirebaseAuth,
+      Firestore: Firestore
+    }}>
+      {children}
+    </FirebaseContext.Provider>
+  )
+}
+
+
+// ============================ Firebase Hooks ============================
+
+const useFirebase = () => useContext(FirebaseContext)
+
+/**
+ * Firebase auth hook for accessing the data stored in the FirebaseContext Provider
+ * @returns Auth object containing the user credentials and auth-related functions
+ */
+export const useFirebaseAuth = () => useFirebase().Auth
+
+/**
+ * Firebase firestore hook for accessing the data stored in the FirebaseContext Provider
+ * @returns Firestore object containing the database data and database-related functions
+ */
+export const useFirestore = () => useFirebase().Firestore
+
+/**
+ * interface of the object returned from the useAssessmentDb hook
+ */
+export interface IAssessmentDoc {
+  sems: string[]
+  terms: string[]
+  subjects: {
+    name: string
+    sem: string         // key that's going to be filtered 
+    assessments: IAssessmetItem[]
+  }[]
+}
+/**
+ * an assessment item type interface of the type/category of the assessment and the items under it
+ * - `name:` term name, must be found inside the term array
+ * - `items:` array of objects containing all the assessments that are under the term category/filter
+ * - - `name:` name of the assessment
+ * - - `grade:` score or grade received for the assessment
+ */
+export interface IAssessmetItem {
+  name: string
+  items: {
+    name: string
+    grade: number | null
+    type: string
+    term: string          // key that's going to be filtered
+    [x: string]: number | string | null
+  }[]
+}
+
+/**
+ * Assessment db hook used to access and restructure the data from the firestore into a component usable object
+ * @returns object that is compatible to be used by the assessment component
+ */
+export const useAssessmentDb = (): IAssessmentDoc => {
+  const unStructuredDoc = useFirebase().Firestore.Firestore
+  
+  // return an empty array if the firestore context is null or undefined
+  if (!unStructuredDoc) return {} as IAssessmentDoc
+
+  // initialize an array of sems
+  let sems = unStructuredDoc.sems
+
+  // initialize an array of terms
+  let terms = unStructuredDoc.terms
+
+  return {
+    sems,
+    terms,
+    subjects: unStructuredDoc.subjects?.map(subject => {
+      return {
+        name: subject.name,
+        sem: subject.sem,
+        assessments: terms.map(termName => {
+          return {
+            name: termName,
+            items: subject.assessments.filter(assessment => assessment.term === termName).map(assessment => {
+              // let { type, ...copy } = assessment
+              return assessment
+            })
+          }
+        })
+      }
+    })
+  }
+}
