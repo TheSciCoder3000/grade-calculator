e85212bdbbeb93d47c6bb3ef306065184bc2205f
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-12 23:02:40 +0800

    refactoring toggler props

--- src/components/Calculator/Toggler/index.tsx
+++ src/components/Calculator/Toggler/index.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 interface ITogglerProps {
     className?: string
-    items: string[]
+    items: { name: string, id: string }[]
+    activeItem: string
     addItemHandler: (fieldName: string) => any
 }
-const Toggler: React.FC<ITogglerProps> = ({ className, items, addItemHandler }) => {
+const Toggler: React.FC<ITogglerProps> = ({ className, activeItem, items, addItemHandler }) => {
   return (
     <div className={`toggler-cont ${className}`}>
-        {items.map((item, indx) => (
-            <div key={`${item}-${indx}`} className="item-cont">
-                {item}
+        {items.map((item) => (
+            <div key={`${item.name}-${item.id}`} className={`item-cont ${(activeItem === item.id) && 'active-item'}`}>
+                {item.name}
             </div>
         ))}
         <button className="add-item-cont">+</button>
