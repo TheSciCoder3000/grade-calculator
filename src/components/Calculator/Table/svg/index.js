cbcaf1bcd372cbe7cf6b6fe109a742558885b9e4
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-22 15:29:56 +0800

    check svg

--- src/components/Calculator/Table/svg/index.js
+++ src/components/Calculator/Table/svg/index.js
@@ -1,3 +1,4 @@
 export { default as SortDown } from "./SortDown";
 export { default as SortUp } from "./SortUp";
 export { default as Sort } from "./Sort";
+export { default as Check } from "./Check";
