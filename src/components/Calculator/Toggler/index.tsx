aba36258ec29abdc7a19140cb445713c4cfe7d79
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-23 00:15:07 +0800

    toggler renaming update

--- src/components/Calculator/Toggler/index.tsx
+++ src/components/Calculator/Toggler/index.tsx
@@ -16,14 +16,18 @@ const Toggler: React.FC<ITogglerProps> = ({
   updateItemHandler, 
   onItemClick 
 }) => {
+  // ================================== Item Creation ================================== 
+  // Input Field creation states
   const [toggleFieldInput, setToggleFieldInput] = useState(false)
   const [fieldInputText, setFieldInputText] = useState('')
   const fieldInput = useRef<HTMLInputElement>(null)
 
+  // Focus onto the input field upon rendering
   useEffect(() => {
     if (toggleFieldInput) fieldInput.current?.focus()
   }, [toggleFieldInput])
 
+  // used to hide input field and set empty field text
   const resetInputField = () => {
     setToggleFieldInput(false)
     setFieldInputText('')
@@ -36,13 +40,50 @@ const Toggler: React.FC<ITogglerProps> = ({
     } else if (e.key === 'Escape') resetInputField()
   }
 
+
+  // ================================== Item Rename ==================================
+  const editFields = useRef<Record<string, HTMLInputElement>>({})
+  const [editMode, setEditMode] = useState<string | null>(null)
+
+  const dbClickHandler = (itemId: string, itemName: string) => {
+    let editField = editFields.current[itemId]
+    if (!editField) return
+    setEditMode(itemId)
+    editField.value = itemName
+  }
+
+  const addEditFieldRefs = (el: HTMLInputElement | null, itemId: string) => {
+    if (el && !Object.values(editFields.current).includes(el)) 
+      editFields.current[itemId] = el
+  }
+
+  const onEditKeyHandler = (e: React.KeyboardEvent<HTMLInputElement>,itemId: string) => {
+    if (e.key === 'Enter') {
+      setEditMode(null)
+      updateItemHandler(itemId, editFields.current[itemId].value)
+    } else if (e.key === 'Escape') setEditMode(null)
+  }
+
+  useEffect(() => {
+    if (editMode && editFields.current) editFields.current[editMode].focus() 
+  }, [editMode])
+
   return (
     <div className={`toggler-cont ${className}`}>
         {items.map((item) => (
           <div key={`${item.name}-${item.id}`} 
-            onClick={() => onItemClick(item.id)} 
+            onClick={() => onItemClick(item.id)}
+            onDoubleClick={() => dbClickHandler(item.id, item.name)}
             className={`item-cont ${(activeItem === item.id) && 'active-item'}`}>
-            {item.name}
+
+            {editMode === item.id ? '' : item.name}
+            <input 
+              type="text" 
+              ref={el => addEditFieldRefs(el, item.id)}
+              onBlur={() => setEditMode(null)} 
+              style={{ display: editMode === item.id ? 'block' : 'none' }}
+              onKeyDown={e => onEditKeyHandler(e, item.id)} />
+
           </div>
         ))}
         {toggleFieldInput && (
