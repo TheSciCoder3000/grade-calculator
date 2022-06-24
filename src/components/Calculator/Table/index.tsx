3c8009eb8f183ee67295d8520a898dcd1338ebda
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-25 00:37:19 +0800

    refactoring columns data for dynamic processing

--- src/components/Calculator/Table/index.tsx
+++ src/components/Calculator/Table/index.tsx
@@ -1,157 +1,109 @@
-import { ISubjects } from 'Firebase/FirebaseDb'
-import { useMemo, FC } from 'react'
-import { useTable, useSortBy, useRowSelect, CellProps, Column } from 'react-table'
-import Checkbox from './Checkbox'
-import CourseCellLink from './CourseLink'
-import SortIcon from './SortIcon'
-import './Table.css'
-
-// type $FixMe = any
-interface IRow {
-  original: ISubjects
-  values: ISubjects
-}
-
-
-interface IColumn {
-  Header: string
-  accessor: string
-  Cell?: string | ((cell: { row: IRow }) => JSX.Element | string)
-  Footer: string | ((row: { rows: IRow[] }) => JSX.Element | string)
-}
+import { ISubjects } from "Firebase/FirebaseDb";
+import { useMemo, FC } from "react";
+import { useTable, useSortBy, useRowSelect, CellProps, Column } from "react-table";
+import Checkbox from "./Checkbox";
+import SortIcon from "./SortIcon";
+import "./Table.css";
+import { createColumns } from "./utils";
 
 interface ITableProps {
-    DATA: ISubjects[]
+    DATA: ISubjects[];
 }
 
-const COLUMNS: IColumn[] = [
-  {
-      Header: 'Course Name',
-      accessor: 'name',
-      Cell: (cell) => {
-        const value = cell.row.original
-        return (
-          <CourseCellLink courseName={value.name} courseId={value.id} />
-        )
-      },
-      Footer: 'Total'
-  },
-  {
-      Header: 'Midterm',
-      accessor: 'mid',
-      Footer: (cell) => {
-        const midSum = cell.rows.reduce((partialSum, row) => {
-          return partialSum + row.values.mid
-        }, 0)
-        return (
-          <>{(midSum/cell.rows.length).toFixed(2)}</>
-        )
-      }
-  },
-  {
-      Header: 'Finals',
-      accessor: 'final',
-      Footer: ({ rows }) => {
-        const finalSum = rows.reduce((partialSum, row) => {
-          return partialSum + row.values.final
-        }, 0)
-        return (
-          <>{Math.round(finalSum/rows.length)}</>
-        )
-      }
-  },
-]
+const GradeTable: FC<ITableProps> = ({ DATA }) => {
+    // Initialize columns
+    const columns = useMemo(() => createColumns(DATA) as Column<ISubjects>[], [DATA]);
 
-const GradeTable: FC<ITableProps> = (props) => {
-  // Initialize columns
-  const columns = useMemo(() => COLUMNS as Column<ISubjects>[], [])
+    // Intialize Data
+    const data = useMemo(() => DATA, [DATA]);
 
-  // Intialize Data
-  const data = useMemo(() => props.DATA, [props.DATA])
+    // Initialize table props from react table
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        footerGroups,
+        selectedFlatRows,
+    } = useTable({ columns, data }, useSortBy, useRowSelect, (hooks) => {
+        hooks.visibleColumns.push((columns) => {
+            return [
+                {
+                    id: "selection",
+                    Header: ({ getToggleAllRowsSelectedProps }) => {
+                        return (
+                            <Checkbox
+                                {...getToggleAllRowsSelectedProps()}
+                                rowId={"header"}
+                                header={true}
+                                className="header-checkbox"
+                            />
+                        );
+                    },
+                    Cell: ({ row }: React.PropsWithChildren<CellProps<ISubjects, any>>) => {
+                        return <Checkbox {...row.getToggleRowSelectedProps()} rowId={row.original.id} />;
+                    },
+                },
+                ...columns,
+            ];
+        });
+    });
 
-  // Initialize table props from react table
-  const {
-      getTableProps,
-      getTableBodyProps,
-      headerGroups,
-      rows,
-      prepareRow,
-      footerGroups,
-      selectedFlatRows
-  } = useTable(
-    { columns, data },
-    useSortBy,
-    useRowSelect,
-    (hooks) => {
-      hooks.visibleColumns.push((columns) => {
-        return [
-          {
-            id: 'selection',
-            Header: ({ getToggleAllRowsSelectedProps }) => {
-              return <Checkbox 
-                        {...getToggleAllRowsSelectedProps()} 
-                        rowId={'header'} 
-                        header={true} className='header-checkbox' />
-            },
-            Cell: ({ row }: React.PropsWithChildren<CellProps<ISubjects, any>>) => {
-              return <Checkbox {...row.getToggleRowSelectedProps()} rowId={row.original.id} />
-            }
-          },
-          ...columns
-        ]
-      })
-    }
-  )
-
-
-  return (
-      <table {...getTableProps({ className: 'calculator__table' })}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column, indx) => (
-                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                    <div className="table-data-cont">
-                      {column.render('Header')}
-                      <span>
-                        {column.id !== 'selection' && (
-                          <SortIcon key={indx}
-                            sort={column.isSorted ? (column.isSortedDesc ? 'desc' : 'asc') : ''} />
-                        )}
-                      </span>
-                    </div>
-                  </th>
+    return (
+        <table {...getTableProps({ className: "calculator__table" })}>
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column, indx) => (
+                            <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                <div className="table-data-cont">
+                                    {column.render("Header")}
+                                    <span>
+                                        {column.id !== "selection" && (
+                                            <SortIcon
+                                                key={indx}
+                                                sort={
+                                                    column.isSorted
+                                                        ? column.isSortedDesc
+                                                            ? "desc"
+                                                            : "asc"
+                                                        : ""
+                                                }
+                                            />
+                                        )}
+                                    </span>
+                                </div>
+                            </th>
+                        ))}
+                    </tr>
                 ))}
-              </tr>
-            ))}
-          </thead>
+            </thead>
 
-          <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-              prepareRow(row)
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  ))}
-                </tr>
-              )
-            })}
-          </tbody>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => (
+                                <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                            ))}
+                        </tr>
+                    );
+                })}
+            </tbody>
 
-          <tfoot>
-            {footerGroups.map(footerGroup => (
-              <tr {...footerGroup.getFooterGroupProps()}>
-                {footerGroup.headers.map(column => (
-                  <td {...column.getFooterProps()}>
-                    {column.render('Footer')}
-                  </td>
+            <tfoot>
+                {footerGroups.map((footerGroup) => (
+                    <tr {...footerGroup.getFooterGroupProps()}>
+                        {footerGroup.headers.map((column) => (
+                            <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+                        ))}
+                    </tr>
                 ))}
-              </tr>
-            ))}
-          </tfoot>
-      </table>
-  )
-}
+            </tfoot>
+        </table>
+    );
+};
 
-export default GradeTable
+export default GradeTable;
---
+++ src/components/Calculator/Table/utils.tsx
@@ -0,0 +1,63 @@
+import { ISubjects } from "Firebase/FirebaseDb";
+import CourseCellLink from "./CourseLink";
+
+interface IRow {
+    original: ISubjects;
+    values: {
+        name: string;
+        [field: string]: string | number;
+    };
+}
+
+interface IColumn {
+    Header: string;
+    accessor: string | ((doc: ISubjects) => number | string);
+    Cell?: string | ((cell: { row: IRow }) => JSX.Element | string);
+    Footer?: string | ((row: { rows: IRow[] }) => JSX.Element | string);
+}
+
+type CreateColumnType = (subjects: ISubjects[]) => IColumn[];
+export const createColumns: CreateColumnType = (subjects) => {
+    const blueprint = subjects[0] || {
+        grades: [
+            {
+                name: 'Midterm',
+                value: 0
+            }
+        ]
+    };
+    return [
+        {
+            Header: "Course Name",
+            accessor: "name",
+            Cell: (cell) => {
+                const value = cell.row.original;
+                return <CourseCellLink courseName={value.name} courseId={value.id} />;
+            },
+            Footer: "Average",
+        },
+        ...(blueprint.extra
+            ? blueprint.extra.map((extra, indx) => {
+                return {
+                    Header: extra.name,
+                    accessor: (doc) => doc.extra?.find(item => item.name === extra.name)?.value,
+                    Cell: ({ row }) => row.values[extra.name]
+                } as IColumn
+              })
+            : []),
+        ...blueprint.grades.map(grade => {
+            return {
+                Header: grade.name,
+                accessor: (doc) => doc.grades.find(item => item.name === grade.name)?.value,
+                Cell: ({ row }) => row.values[grade.name],
+                Footer: ({ rows }) => {
+                    const sum = rows.reduce((partialSum, row) => {
+                        const rowVal = row.values[grade.name] as number
+                        return partialSum + rowVal
+                    }, 0)
+                    return <>{Math.round(sum/rows.length)}</>
+                }
+            } as IColumn
+        })
+    ];
+};
