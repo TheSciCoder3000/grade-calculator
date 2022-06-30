57c8a74eb8de1735e6b90b4fc8e0eef370eff2e8
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2022-06-30 12:50:16 +0800

    removed custom checkbox styling

--- src/components/Calculator/Table/Checkbox.tsx
+++ src/components/Calculator/Table/Checkbox.tsx
@@ -1,29 +1,48 @@
-import React, { MutableRefObject, useState } from 'react'
-import { TableToggleCommonProps } from 'react-table'
+import React, { MutableRefObject, useState } from "react";
+import { TableToggleCommonProps } from "react-table";
 
 // type InputProps = React.HTMLProps<HTMLInputElement>
 
 interface CheckboxProps extends TableToggleCommonProps {
-    rowId: string,
-    header?: boolean
+    rowId: string;
+    header?: boolean;
 }
-const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(({ indeterminate, rowId, header, ...rest }, ref) => {
-    const defaultRef = React.useRef<HTMLInputElement>(null)
-    const resolvedRef = (ref as MutableRefObject<HTMLInputElement>) || defaultRef
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+    ({ indeterminate, rowId, header, ...rest }, ref) => {
+        const defaultRef = React.useRef<HTMLInputElement>(null);
+        const resolvedRef = (ref as MutableRefObject<HTMLInputElement>) || defaultRef;
 
-    React.useEffect(() => {
-        if (resolvedRef) resolvedRef.current.indeterminate = !!indeterminate
-    }, [resolvedRef, indeterminate])
+        React.useEffect(() => {
+            if (resolvedRef) resolvedRef.current.indeterminate = !!indeterminate;
+        }, [resolvedRef, indeterminate]);
 
-    
-    return (
-        <div className='check-cont'>
-            <input  id={`checkbox-${rowId}`} type="checkbox" ref={resolvedRef} {...rest} />
-            <label className={header ? 'header-check' : ''} htmlFor={`checkbox-${rowId}`}>
-                {resolvedRef.current?.checked && <>&#10004;</>}
-            </label>
-        </div>
-    )
-})
+        return (
+            <div className="check-cont">
+                <input
+                    id={`checkbox-${rowId}`}
+                    className={header ? "header-checkbox" : ""}
+                    type="checkbox"
+                    ref={resolvedRef}
+                    {...rest}
+                />
 
-export default Checkbox
+                {/* TODO: fix custom checkbox */}
+                {/* <label
+                    className={header ? "header-check" : ""}
+                    htmlFor={`checkbox-${rowId}`}
+                    onClick={(e) => {
+                        console.log(resolvedRef.current);
+                        //@ts-ignore
+                        resolvedRef.current.checked = !checked;
+
+                        console.log(resolvedRef.current);
+                    }}
+                >
+                    {checked && <>&#10004;</>}
+                </label> */}
+            </div>
+        );
+    }
+);
+
+export default Checkbox;
